469e48d6f5f809fefb9c8281592ad66753489cbf
Andrew Wayne Mayo <andrewmayo@college.harvard.edu>
2015-04-17 04:16:15 -0400

    main.ml file created

---
+++ main.ml
@@ -0,0 +1,23 @@
+open Core.Std
+open dict
+
+exception TODO
+
+(* Read in .csv file from user input and store in adjacency list *)
+let read_csv : fun () -> () = TODO ;;
+
+(* Request start and finish nodes from user *)
+let get_nodes : fun () -> string * string = 
+  let st = read_string () in
+  let fin = read_string () in
+  st, fin ;;
+
+(* Run dijkstra's algorithm to find shortest path between start and finish *)
+let dijkstra () : node list =
+  let (st,fin) = get_nodes in
+  (* match strings with nodes in adjacency list  and then run algorithm 
+   * using fibonocci heap for storage and access *)
+  TODO
+
+read_csv () ;;
+dijkstra () ;;
