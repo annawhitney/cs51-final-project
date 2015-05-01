6b43be29c92916f8ed8909ca7b102d4605500297
Anna Whitney <awhitney@college.harvard.edu>
2015-05-01 08:38:30 -0400

    more compile errors in Heap.ml addressed

--- Heap.ml
+++ Heap.ml
@@ -629,14 +629,7 @@ module GeoGraph : (GRAPH with type node = GeoNode.node
     Graph(GeoNode);;
 
 (* code for reading in the csv file *)
-
-(* NOTE: I'm pretty sure that the graph is a functional data structure (the
- * only thing in it than can be updated are bits of individual nodes, but
- * nodes can't be added imperatively), so we shouldn't be ignoring the result
- * of addhelper - that's the new graph that actually contains the nodes we
- * just added. I'm not going to focus on this just yet, but I'm fairly certain
- * that this is a problem we need to fix before the whole thing will work. *)
-let rec addedges (castlist: (string * (float * float)) list) 
+let addedges (castlist: (string * (float * float)) list) 
 (graph: GeoGraph.graph) (cutoff: float) : GeoGraph.graph = 
   let rec addhelper (place: string * (float * float)) 
   (rest: (string * (float * float)) list) (graph: GeoGraph.graph)
@@ -647,32 +640,37 @@ let rec addedges (castlist: (string * (float * float)) list)
         (let (name1,loc1) = place in
         match Distance.distance cutoff loc1 loc2 with
         | None -> addhelper place tl graph
-        | Some d -> GeoGraph.add_edge graph
-                    (GeoNode.node_of_tag name1)
-                    (GeoNode.node_of_tag name2) d; 
-                    addhelper place tl graph)
+        | Some d ->
+            let newgraph = GeoGraph.add_edge graph
+                          (GeoNode.node_of_tag name1)
+                          (GeoNode.node_of_tag name2) d 
+            in
+            addhelper place tl newgraph)
   in
   match castlist with
   | [] -> graph
-  | hd::tl -> ignore(addhelper hd tl graph); addedges tl graph cutoff
+  | hd::tl -> addhelper hd tl graph
 
 let read_csv () : GeoGraph.graph =  
-  let usage () = Printf.print "usage: %s csv cutoff " Sys.argv.(0); exit 1 in 
+  let usage () = Printf.printf "usage: %s csv cutoff " Sys.argv.(0); exit 1 in 
   if Array.length Sys.argv <> 3 then usage () ;
   (* file pointer - just like in CS50! *)
   let cutoff = Sys.argv.(2) in
-  let file = create Sys.argv.(1) in
-  let lines = input_lines file in
-  let delimiter = Str.regex ";" in
+  let file = In_channel.create Sys.argv.(1) in
+  let lines = In_channel.input_lines file in
+  let delimiter = Str.regexp ";" in
   let parse_line line = Str.split_delim delimiter line in
   let parsed : string list list = List.map lines ~f:parse_line in
-  let rec cast (parselist: string list list) : string * (float * float) list =
+  let rec cast (parselist: string list list) :
+      (string * (float * float)) list =
      match parselist with
      | [] -> []  
-     | hd::tl -> (match hd with 
-       | name::lat::long -> name * 
-         ((Float.of_string lat) * (Float.of_string long))::(casted tl)
-       | _ -> []) in
+     | hd::tl ->
+         (match hd with 
+         | [name;lat;lng] ->
+             (name * ((Float.of_string lat)*(Float.of_string lng)))::(cast tl)
+         | _ -> [])
+  in
   let casted = cast parsed in
   addedges casted GeoGraph.empty cutoff
 
